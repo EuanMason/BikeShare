dfcc698c9d02135ab7183fece607a74f96e8a77c
Alberto <2556613S@student.gla.ac.uk>
2021-03-02 10:16:29 +0000

    Added comments on sqlquery and decorator python file

--- bikeshare/util/decorators.py
+++ bikeshare/util/decorators.py
@@ -7,35 +7,51 @@ from rest_framework.utils import json
 
 
 def auth_required(func):
-    """
-    Authentication decorator
+    """ Authentication decorator
+
+    Args:
+        func (Function): the function to be executed
+
+    Returns:
+        response (Response): The response containing the status
     """
 
     NOPERMISSION = "Login Required"
 
+    # Inner function of decorator
     def decorator(request, *args, **kwargs):
         try:
+            # Try execute function
             if request.COOKIES['userid']:
                 return func(request, *args, **kwargs)
+        # Catch the exceptions
         except ValueError:
             raise HttpResponse(status=status.HTTP_403_FORBIDDEN)
         except KeyError:
             return HttpResponse(status=status.HTTP_403_FORBIDDEN)
         return HttpResponse(status=status.HTTP_403_FORBIDDEN)
 
+    # Return decorator 
     return decorator
 
 
 def role_check(roles: list, func=None):
-    """
-    Role checker
+    """ Role checker
+
+    Args:
+        func (Function): the function to be executed
+
+    Returns:
+        response (Response): The response containing the status
     """
 
     def decorator(func):
+        # Inner function to validate the role
         @wraps(func)
         def returned_wrapper(request, *args, **kwargs):
             try:
                 for role in roles:
+                    # Check the roles
                     if request.COOKIES['role'] and request.COOKIES['role'] == role:
                         return func(request, *args, **kwargs)
             except ObjectDoesNotExist:
@@ -43,8 +59,9 @@ def role_check(roles: list, func=None):
             return HttpResponse(status=status.HTTP_403_FORBIDDEN)
 
         return returned_wrapper
-
+    # Return function based on existence of func param
     if not func:
+        # If not exists then create a dummy function to retunr the previous decorator
         def foo(func):
             return decorator(func)
 
--- bikeshare/util/sql_query.py
+++ bikeshare/util/sql_query.py
@@ -2,22 +2,35 @@ import sqlite3
 from bikeshareapp.models import *
 
 
-# Check if the user exist,
-# param: userid (login name -- Email, unique Identifier)
-# return: bool
 def check_user(userid: str) -> bool:
+    """ Check if the user exist,
+
+    Args:
+        userid (string): Email, unique Identifier
+
+    Returns:
+        bool (Response): Boolean indicating the validation
+    """
+
     try:
+        # Try to get the user
         user = User.objects.get(userid=userid)
         return True
     except models.ObjectDoesNotExist:
         return False
 
-
-# Check the user login info
-# params: str userid (login name -- Email, unique Identifier)
-#         str password
-# return: user instance or int state code
+ 
 def login_check(userid: str, password: str):
+    """ Check the user login info
+
+    Args:
+        userid (string): Email, unique Identifier
+        password (string): Password
+    Returns:
+        user (Object): user instance or int state code
+    """
+
+    # Check if user exists
     exist = check_user(userid)
     if not exist:
         return 2  # state code 2: username not exist
@@ -28,44 +41,78 @@ def login_check(userid: str, password: str):
 
 
 def get_all_locations():
+    """ Get locations
+
+    Args:
+        None
+    Returns:
+        address_list (list): list of locations
+    """
+
     try:
+        # Get all locations
         locations = Address.objects.all()
         address_list = []
+        # Iterate over locations to form list
         for location in locations:
             line1 = location.Line1
             address_list.append(line1)
+        # Format list
         address_list = list(set(address_list))
-        print(address_list)
+        # print(address_list)
         return address_list
     except:
         return None
 
 
 def get_all_bikes_locations():
+    """ Get bikes locations
+
+    Args:
+        None
+    Returns:
+        address_list (list): list of locations
+    """
+
     try:
+        # Get all bikes
         bike_list = Bike.objects.all()
         address_list = {}
+        # Iterate over list of bikes to get all the locations
         for bike in bike_list:
             location_id = bike.AddressLocationID
             location = location_id.Line1
             if location in address_list.keys():
+                # Count the ocurrences of the address
                 address_list[location] += 1
             else:
+                # If address does not extist, add a new one
                 address_list[location] = 1
-
+        # Retunr the formed list
         return address_list
     except:
         return None
 
 
 def get_all_unavailable_bikes():
+    """ Get not available bikes
+
+    Args:
+        None
+    Returns:
+        bike_list (list): list of bikes
+    """
+
     try:
+        # Get all the not available bikes
         bikes = Bike.objects.filter(IsAvailable=0)
         bike_list = []
+        # Iterate to create id list
         for bike in bikes:
             bike_list.append(bike.BikeID)
         bike_list = list(set(bike_list))
-        print(bike_list)
+        # print(bike_list)
+        # Return the bikes ids
         return bike_list
     except:
         return None
